b11aa9fd92acc7e56f7a1af9701113f0422b09a8
tishan <tishanshavindas@gmail.com>
2024-10-29 22:58:45 -0700

    include all css parts

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,6 @@ app.use('/api/lecturehalls', require('./routes/lecturehall')); // Add this line
 app.use('/api/bookingrequests', require('./routes/bookingrequests'));
 app.use('/api/labs', require('./routes/labs'));
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
